560797e47d862c005baf19aeb167dea20c963fc0
vineetsingh0809 <ankitchilma@gmail.com>
2022-10-13 15:03:02 +0530

    Create Tax_in_ChefLand.cpp

---
+++ Tax_in_ChefLand.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int x;
+        cin>>x;
+
+        if (x > 100)
+        {
+            x -= 10;
+        }
+        cout<<x<<endl;
+    }
+    return 0;
+}
