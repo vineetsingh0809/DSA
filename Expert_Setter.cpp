b91d669405dad150b17055e3f65b4495ff863c64
vineetsingh0809 <ankitchilma@gmail.com>
2022-10-14 18:51:34 +0530

    Create Expert_Setter.cpp

---
+++ Expert_Setter.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        int ans = x - y;
+        if (ans <= y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
