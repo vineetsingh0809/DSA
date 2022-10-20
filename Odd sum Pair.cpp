20b65aab7f32d8c9bf72dc5cb0056d5fc592119c
vineetsingh0809 <ankitchilma@gmail.com>
2022-10-20 17:51:13 +0530

    Create Odd sum Pair.cpp

---
+++ Odd sum Pair.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int x = a + b;
+        int y = b + c;
+        int z = c + a;
+
+        if (x % 2 != 0 || y % 2 != 0 || z % 2 != 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
