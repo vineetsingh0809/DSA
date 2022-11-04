6e4087d50d19fe1c704b64f0abfc994353205d79
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-04 19:22:25 +0530

    Create Roller_Coaster.cpp

---
+++ Roller_Coaster.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, h;
+        cin >> x >> h;
+
+        if (x >= h)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
