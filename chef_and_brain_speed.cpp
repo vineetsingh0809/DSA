ad4afe16ea72cc5c12558eaaf8d6879c32753bba
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-04 19:22:09 +0530

    Create chef_and_brain_speed.cpp

---
+++ chef_and_brain_speed.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if (y > x)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
