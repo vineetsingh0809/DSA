547216dad18fba583efd884731a5cc9ab6b5bdf4
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-09 15:53:54 +0530

    Create number_of_credits.cpp

---
+++ number_of_credits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        cout << (x * 4) + (y * 2) << endl;
+    }
+    return 0;
+}
