febb7cdd2a77928656c896ff2e3558f641cc3540
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-09 15:53:40 +0530

    Create chairs_reuirements.cpp

---
+++ chairs_reuirements.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (y > x)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << x - y << endl;
+        }
+    }
+    return 0;
+}
