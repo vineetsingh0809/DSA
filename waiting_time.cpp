7f533a90bb4790075145fb1303b4ce222421d565
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-10 18:57:03 +0530

    Create waiting_time.cpp

---
+++ waiting_time.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int k,x;
+        cin>>k>>x;
+
+        int mul = k*7;
+
+        cout<<(mul-x)<<endl;
+    }
+    return 0;
+}
