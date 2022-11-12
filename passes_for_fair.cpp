df93f2c10cc7d06a1c77094b4ca7f9dff255c268
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-12 20:27:31 +0530

    Create passes_for_fair.cpp

---
+++ passes_for_fair.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        if (k >= n + 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
