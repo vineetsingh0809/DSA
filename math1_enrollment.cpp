c7b24f96ff10023c9cfb67f5b4f3a58cb0aa8fe4
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-16 17:15:47 +0530

    Create math1_enrollment.cpp

---
+++ math1_enrollment.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (y - x > 0)
+        {
+            cout << (y - x) << endl;
+        }
+        else
+        {
+            cout << 0 << endl;
+        }
+    }
+    return 0;
+}
