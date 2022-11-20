8e6a25e10e0fbac78429754da5ef2d19a1452abc
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-20 17:28:59 +0530

    Create minimum_coins.cpp

---
+++ minimum_coins.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        
+        int ans = x%10;
+        cout<<ans<<endl;
+    }
+	return 0;
+}
