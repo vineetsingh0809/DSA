aececca3c6fb3b81d75bd5b89b57b4774564b067
vineetsingh0809 <ankitchilma@gmail.com>
2022-11-21 19:40:24 +0530

    Create true_and_false_paper.cpp

---
+++ true_and_false_paper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        cout << (n - k) << endl;
+    }
+    return 0;
+}
