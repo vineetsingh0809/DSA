200e56a44f9b27fd48b5e30f0272c958500f3333
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-11-24 15:38:35 +0530

    Create monopoly_in_chefland.cpp

---
+++ monopoly_in_chefland.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int r1, r2, r3;
+        cin >> r1 >> r2 >> r3;
+
+        if (r1 > r2 + r3 || r2 > r1 + r3 || r3 > r1 + r2)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
