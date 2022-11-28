1a8dc22d4e17738cab67f5ef19ded6f7063bbeb7
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-11-28 14:01:40 +0530

    Create football_cup.cpp

---
+++ football_cup.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (x == 0 && y == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            if (x == y)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+    return 0;
+}
