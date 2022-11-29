c96d61e9e2b1443f595383d13acbf93823575993
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-11-29 18:15:05 +0530

    Create speciality.cpp

---
+++ speciality.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        if (x > y && x > z)
+        {
+            cout << "Setter" << endl;
+        }
+        else if (y > x && y > z)
+        {
+            cout << "Tester" << endl;
+        }
+        else
+        {
+            cout << "Editorialist" << endl;
+        }
+    }
+    return 0;
+}
