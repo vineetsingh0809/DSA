14ef06873c0bee7633042eacab840d28095cf43e
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-12-02 16:10:27 +0530

    Create tour_of-king.cpp

---
+++ tour_of-king.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int total = (n * 5) + (m * 7);
+        cout << total << endl;
+    }
+    return 0;
+}
