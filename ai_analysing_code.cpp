a340ee9a7d3f17280a5f0ef3c8dcc1c93a289202
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-12-02 16:09:46 +0530

    Create ai_analysing_code.cpp

---
+++ ai_analysing_code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    cin >> c;
+
+    if (c <= 1000)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
