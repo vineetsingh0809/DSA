6eca873077e79a6e29f18bb482d61343f1cf340d
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-12-05 10:57:15 +0530

    Create tyre_problem.cpp

---
+++ tyre_problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int ans = (n * 2) + (m * 4);
+        cout << ans << endl;
+    }
+    return 0;
+}
