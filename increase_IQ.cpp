4fb9b76de38f6cb5a06241964225b93cba34e0ae
Vineet Singh <75788774+vineetsingh0809@users.noreply.github.com>
2022-12-06 17:16:16 +0530

    Create increase_IQ.cpp

---
+++ increase_IQ.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x + 7 > 170)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
