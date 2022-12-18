f2249ef1d27cb49e4f14386d128eeeebbf61b852
vineetsingh0809 <ankitchilma@gmail.com>
2022-12-18 18:43:46 +0530

    Create Rain_in_chefland.cpp

---
+++ Rain_in_chefland.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        if (x < 3)
+        {
+            cout << "LIGHT" << endl;
+        }
+        else if (x >= 3 && x < 7)
+        {
+            cout << "MODERATE" << endl;
+        }
+        else
+        {
+            cout << "HEAVY" << endl;
+        }
+    }
+    return 0;
+}
